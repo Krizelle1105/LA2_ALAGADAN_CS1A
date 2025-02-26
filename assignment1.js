cd107a997a15b4dceb02152d9123e90e81b01625
Krizelle1105 <krizelleannalagadan@gmail.com>
2025-02-26 12:41:06 +0800

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,31 @@
+let myFirstName_MiddleName_LastName = "Krizelle Ann Alcantara Alagadan";
+let myBirthdate = "November 11, 2005";
+let myBirthplace = "Parioc Segundo, Candon City, Ilocos Sur, 2710";
+let myAddress = "Parioc Segundo, Candon City, Ilocos Sur, 2710";
+let myCourse = "Bachelor of Science in Computer Science";
+let myDreamJob = "Business Owner";
+
+console.log(`[${myFirstName_MiddleName_LastName.toUpperCase()}] was born [${myBirthdate}] at [${myBirthplace.toUpperCase()}]
+and currently living at [${myAddress.toUpperCase()}] is currently taking [${myCourse.toUpperCase()}] and want to be [${myDreamJob.toUpperCase()}]`);
+
+//First Classmate
+let theirFirstName_MiddleName_LastName1 = "Nedelyn Fernandez Donato";
+let theirBirthdate1 = "December 21, 2006";
+let theirBirthplace1 = "Sorioan, Salcedo, Ilocos Sur, 2711";
+let theirAddress1 = "Sorioan, Salcedo, Ilocos Sur, 2711";
+let theirCourse1 = "Bachelor of Science in Computer Science";
+let theirDreamJob1 = "Software Engineer";
+
+console.log(`[${theirFirstName_MiddleName_LastName1.toUpperCase()}] was born [${theirBirthdate1}] at [${theirBirthplace1.toUpperCase()}]
+and currently living at [${theirAddress1.toUpperCase()}] is currently taking [${theirCourse1.toUpperCase()}] and want to be [${theirDreamJob1.toUpperCase()}]`);
+
+//Second Classmate
+let theirFirstName_MiddleName_LastName2 = "Jhamaica Mae Gines Sindon";
+let theirBirthdate2 = "January 20, 2006";
+let theirBirthplace2 = "Dasay, Narvacan, Ilocos Sur, 2704";
+let theirAddress2 = "Dasay, Narvacan, Ilocos Sur, 2704";
+let theirCourse2 = "Bachelor of Science in Computer Science";
+let theirDreamJob2 = "Software Engineering";
+
+console.log(`[${theirFirstName_MiddleName_LastName2.toUpperCase()}] was born [${theirBirthdate2}] at [${theirBirthplace2.toUpperCase()}]
+and currently living at [${theirAddress2.toUpperCase()}] is currently taking [${theirCourse2.toUpperCase()}] and want to be [${theirDreamJob2.toUpperCase()}]`);
